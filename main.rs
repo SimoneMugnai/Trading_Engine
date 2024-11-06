5e37bd4a99a49276502bcef375c7823c7cb2e486
Simone Mugnai <124578718+SimoneMugnai@users.noreply.github.com>
2024-11-07 00:11:05 +0100

    Added price struct and limit

--- main.rs
+++ main.rs
@@ -16,3 +16,38 @@ impl Order{
         Order{bid_or_ask,size}
   }
 }
+
+//I want to put it in an hashmap, so f64 is not great for inconsistent values.
+//So our own price implementation
+#[derive(Debug)]
+struct Price{
+    integral: u64,
+    fractional: u64,
+    scalar: u64
+
+}
+
+impl Price{
+    fn new(price: f64) -> Price{
+        let scalar = 100000;
+        let integral = price as u64;
+        let fractional = ((price % 1.0) * scalar as f64) as u64;
+        Price {
+            scalar,
+            integral,
+            fractional
+
+        }
+
+    }
+}
+struct Limit{
+    price: Price,
+    orders:Vec<Order>
+}
+
+
+fn main(){
+    let price = Price::new(50.5);
+    println!("{:?}",price);
+}
